7128a1ac7e0bb2eea3592fd210524262a2772e0a
lucopdev <lucopdev@gmail.com>
2023-03-07 01:10:27 -0300

    estilização inicial 2

--- src/components/Card.js
+++ src/components/Card.js
@@ -17,14 +17,31 @@ class Card extends React.Component {
 
     return (
       <main className="card-main">
-        <h2 data-testid="name-card">{ cardName }</h2>
-        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
-        <p data-testid="description-card">{ cardDescription }</p>
-        <p data-testid="attr1-card">{ cardAttr1 }</p>
-        <p data-testid="attr2-card">{ cardAttr2 }</p>
-        <p data-testid="attr3-card">{ cardAttr3 }</p>
-        <p data-testid="rare-card">{ cardRare }</p>
-        { cardTrunfo ? <p data-testid="trunfo-card">Super Trunfo</p> : ''}
+        <section className="main-section">
+          <h2 className="card-title" data-testid="name-card">{ cardName }</h2>
+          <div className="img-container">
+            { cardImage ? <img
+              className="card-img"
+              data-testid="image-card"
+              src={ cardImage }
+              alt={ cardName }
+            /> : <div className="empty"> </div> }
+          </div>
+          <p
+            className="description"
+            data-testid="description-card"
+          >
+            { cardDescription }
+          </p>
+          <div className="p-container">
+            <p data-testid="attr1-card">{ cardAttr1 }</p>
+            <p data-testid="attr2-card">{ cardAttr2 }</p>
+            <p data-testid="attr3-card">{ cardAttr3 }</p>
+            <p data-testid="rare-card">{ cardRare }</p>
+          </div>
+          { cardTrunfo
+            ? <p className="trunfo" data-testid="trunfo-card">Super Trunfo</p> : ''}
+        </section>
       </main>
     );
   }
