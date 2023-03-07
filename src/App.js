83ebe813be7d0ede526cf195deb5ca4f887833d3
lucopdev <lucopdev@gmail.com>
2023-03-06 21:42:27 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: 0,
-      cardAttr2: 0,
-      cardAttr3: 0,
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
       cardImage: '',
       cardRare: '',
       cardTrunfo: false,
@@ -21,12 +21,38 @@ class App extends React.Component {
     };
   }
 
+  isPossibleSum = () => {
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    let possibleSum = true;
+
+    if ((Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3)) > 210) possibleSum = false;
+    if (cardAttr1 > 90 || cardAttr2 > 90 || cardAttr3 > 90) possibleSum = false;
+    if (cardAttr1 < 0 || cardAttr2 < 0 || cardAttr3 < 0) possibleSum = false;
+    return possibleSum;
+  };
+  
+  handleErrors = () => {
+    const { cardName, cardDescription, cardImage, cardRare } = this.state;
+    console.log(this.isPossibleSum())
+    const errorsList = [
+      !cardName.length,
+      !cardDescription.length,
+      !cardImage.length,
+      !cardRare.length,
+      !this.isPossibleSum(),
+    ];
+    const noErrors = errorsList.every((error)=> error === false);
+    this.setState({
+      isSaveButtonDisabled: !noErrors,
+    })
+  };
+
   onInputChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, this.handleErrors);
   };
 
   onSaveButtonClick = () => {
